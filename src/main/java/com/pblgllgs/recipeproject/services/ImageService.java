1ff8185975005842adaca297484c691c86365429
pbl.gllgs <pbl.gllgs@gmail.com>
2021-10-22 17:20:06 -0300

    Updates 22/10/2021 17:19

---
+++ src/main/java/com/pblgllgs/recipeproject/services/ImageService.java
@@ -0,0 +1,7 @@
+package com.pblgllgs.recipeproject.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long valueOf, MultipartFile file);
+}
